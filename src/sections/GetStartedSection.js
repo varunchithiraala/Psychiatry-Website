f7d2efb9d4a86269b124bfb61784ac498602d974
varunchithiraala <140340608+varunchithiraala@users.noreply.github.com>
2024-10-21 16:52:56 +0530

    Psychiatry Website Solution

--- src/sections/GetStartedSection.js
+++ src/sections/GetStartedSection.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import { assets } from '../assets/assets';
-
-const steps = [
-  {
-    icon: assets.appointment_icon,
-    number: '01',
-    text: 'Schedule an appointment'
-  },
-  {
-    icon: assets.hanshake_icon,
-    number: '02',
-    text: 'Connect with your provider'
-  },
-  {
-    icon: assets.negotiation_icon,
-    number: '03',
-    text: 'Discuss your comprehensive assessment'
-  },
-  {
-    icon: assets.clipboard_icon,
-    number: '04',
-    text: 'Receive your individualized treatment plan'
-  }
-];
+import { mockData } from '../data/mockData';
 
 const GetStartedSection = () => (
     <section className="relative">
-        <div className="p-8 text-center bg-black border-y-4 border-thicker-gold lg:pt-10 lg:pb-10">
-        <h2 className="mb-3 text-2xl font-semibold text-thick-gold md:text-4xl font-playfair">
+        <div className="p-8 pb-10 text-center bg-black border-y-4 border-thicker-gold lg:pt-10 lg:pb-12">
+        <h2 className="mb-0 text-2xl font-semibold sm:mb-3 text-thick-gold xs:text-3xl md:text-4xl font-playfair">
             Four Simple Steps To
         </h2>
-        <h2 className="mb-6 text-2xl font-semibold md:mb-12 text-thick-gold md:text-4xl font-playfair">
+        <h2 className="mb-6 text-2xl font-semibold md:mb-12 text-thick-gold xs:text-3xl md:text-4xl font-playfair">
             Get Started
         </h2>
         <ul className="flex flex-col xs:flex-row xs:flex-wrap xs:justify-center md:flex-nowrap md:gap-2 lg:gap-8 lg:flex-row">
-            {steps.map((step, index) => (
+            {mockData.steps.map((step, index) => (
             <li key={index} className="mt-6 xs:mt-8 md:mt-0 sm:w-1/2 md:w-auto">
                 <div className="flex flex-col items-center">
                     <img src={step.icon} alt={step.text} className="w-12 h-12" />
@@ -44,8 +22,8 @@ const GetStartedSection = () => (
             </li>
             ))}
         </ul>
-        <div className="flex justify-center mt-2 md:mt-4">
-            <button className="flex items-center px-8 py-2 mt-3 text-sm font-medium text-black rounded-full lg:px-12 lg:py-3 lg:text-base bg-custom-gradient">
+        <div className="flex justify-center mt-4 md:mt-6">
+            <button className="flex items-center px-8 py-2 mt-4 text-sm font-medium text-black rounded-full lg:px-12 lg:py-3 lg:text-base bg-custom-gradient">
             Schedule a Call
             <img src={assets.right_arrow_icon} className="h-3 ml-1 mt-[2px] md:mt-1 md:h-5" alt="right_arrow" />
             </button>
